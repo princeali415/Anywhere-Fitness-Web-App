e81de398650be1a4f2c5eb720daa3c598fa61a88
Dustin Brinkman <mimirs.spring@gmail.com>
2021-01-07 00:14:48 -0500

    errors now display

--- src/components/Register.js
+++ src/components/Register.js
@@ -115,6 +115,10 @@ export default function SignUp(props) {
           </label>
           <br/>
             <button disabled={disabled}>Submit</button>
+            <div className='errors'>
+              <div>{formErrors.email}</div>
+              <div>{formErrors.password}</div>
+            </div>
             
         </form>
       )}
